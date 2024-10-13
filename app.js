4326ed1397147bf227fb57e2b8389a25a5cf2139
PinballgamingYT <153408663+PinballgamingYT@users.noreply.github.com>
2024-10-12 21:10:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const port = process.env.PORT || 3000;
 // Serve static files from the assets directory
 app.use('/assets', express.static(path.join(__dirname, 'pages/assets')));
 
-// Define route for your homepage
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'home.html'));
-});
-
-// Other routes if needed
 app.get('/locations', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages', 'locations.html'));
 });
