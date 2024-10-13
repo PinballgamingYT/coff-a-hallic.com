ea205ca8ab3428429a258da55b732152e16c3b3a
PinballgamingYT <153408663+PinballgamingYT@users.noreply.github.com>
2024-10-12 22:11:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use('/assets/ourteamimages', express.static(path.join(__dirname, 'pages/asse
 // Define route for your homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages', 'home.html'));
+});
 
 // Routes for other pages with exact paths
 app.get('/locations.html', (req, res) => {
